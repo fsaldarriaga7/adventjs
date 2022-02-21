404aa32105665179d3afccaa7ef21312873a7f04
Felipe Saldarriaga <saldarriagafelipe3@gmail.com>
2022-02-21 16:44:47 -0500

    Agregada funcionalidad de buscar ovejas rojas

---
+++ contasOvejas.js
@@ -0,0 +1,16 @@
+const ovejas = [
+    { name: 'Noa', color: 'azul' },
+    { name: 'Euge', color: 'rojo' },
+    { name: 'Navidad', color: 'rojo' },
+    { name: 'Ki Na Ma', color: 'rojo'},
+    { name: 'AAAAAaaaaa', color: 'rojo' },
+    { name: 'Nnnnnnnn', color: 'rojo'}
+]
+
+//Nuevo Array con las ovejas rojas
+let ovejasRojas = ovejas.filter( element => element.color == 'rojo');
+
+console.log (ovejasRojas);
+
+
+
