cb7963c9040a375da5e13fc1d3b52b77780e8a9f
Felipe Saldarriaga <saldarriagafelipe3@gmail.com>
2022-02-21 17:17:43 -0500

    Lista la funcionalidad de contar ovejas por nombre

---
+++ contasOvejas.js
@@ -0,0 +1,33 @@
+const ovejas = [
+    { name: 'Noa', color: 'azul' },
+    { name: 'Euge', color: 'rojo' },
+    { name: 'Navidad', color: 'rojo' },
+    { name: 'Ki Na Ma', color: 'rojo'},
+    { name: 'AAAAAaaaaa', color: 'rojo' },
+    { name: 'Nnnnnnnn', color: 'rojo'}
+]
+
+//Cómo buscar
+console.log (ovejas[0].name.includes('a'));
+console.log (ovejas[0].name.includes('n'));
+
+//Convertir todo a Minúsculas para poder buscar sin importar mayúsculas o minúsculas
+console.log(ovejas[0].name.toLowerCase());
+
+//toLowerCase Funcionando
+console.log (ovejas[0].name.toLowerCase().includes('n'));
+
+
+//Nuevo Array, con las ovejas que incluyen las letras 'a' u 'o' en sus nombres
+let ovejasNombreAN = new Array;
+let i;
+
+console.log(ovejas.length);
+
+for (i=0; i<ovejas.length; i++){
+    if (ovejas[i].name.toLowerCase().includes('a')&&ovejas[i].name.toLowerCase().includes('n')) {
+        ovejasNombreAN.push(ovejas[i]);        
+    }
+}
+
+console.log(ovejasNombreAN);
